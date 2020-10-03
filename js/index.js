ecdde9436b5b0aab911bfeb7323b74f42908d2ae
M. Cullen McClellan <cullenmcclellan@gmail.com>
2020-10-02 20:28:32 -0400

    navLink mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+const navLink = document.querySelectorAll('.nav-link');
+const funBus = document.querySelector('.logo-heading');
+
+navLink[0].addEventListener("mouseover", () => {
+    navLink[0].style.color = "green";
+});
+
+navLink[1].addEventListener("mouseover", () => {
+    navLink[1].style.color = "red";
+});
+
+navLink[2].addEventListener("mouseover", () => {
+    navLink[2].style.color = "gold";
+});
+
+navLink[3].addEventListener("mouseover", () => {
+    navLink[3].style.color = "navy";
+});
+
+//window.addEventListener('scroll', ())
